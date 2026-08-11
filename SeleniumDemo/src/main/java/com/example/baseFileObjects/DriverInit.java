ee3e9ff886c2da2d07af837fec770d69aeca6321
pranchali-qa <pranchaliaher@gmail.com>
2026-08-11 14:23:04 +0530

    Add Firefox driver and options imports

--- SeleniumDemo/src/main/java/com/example/baseFileObjects/DriverInit.java
+++ SeleniumDemo/src/main/java/com/example/baseFileObjects/DriverInit.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class DriverInit{
 
